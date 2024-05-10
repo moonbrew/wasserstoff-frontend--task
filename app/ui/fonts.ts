bed258e0dcc90de4a3e04bf4f261e97fc2e175d1
moonbrew <jossyg@ymail.com>
2024-05-10 22:43:38 +0530

    background done

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Lexend } from 'next/font/google';
+
+export const lexend_700 = Lexend({
+    weight: '700',
+    subsets: ['latin']
+});
+
+export const lexend_800 = Lexend({
+    weight: '800',
+    subsets: ['latin']
+});
