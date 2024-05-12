403d07d05149b0227cf529adfcb77c946f035886
moonbrew <jossyg@ymail.com>
2024-05-12 08:09:26 +0530

    Galley on click animation

--- app/ui/collections-gallery.tsx
+++ app/ui/collections-gallery.tsx
@@ -1,28 +1,74 @@
+'use client'
+import { animated, useSpring } from '@react-spring/web';
 import Image from "next/image";
 
 export default function Gallery() {
-    const images = [{ id: 1, url: "/collections/1.png" }, { id: 2, url: "/collections/2.png" }, { id: 3, url: "/collections/3.png" }];
+    const images = [
+        { id: 1, url: "/collections/1.png" },
+        { id: 2, url: "/collections/2.png" },
+        { id: 3, url: "/collections/3.png" },
+        { id: 4, url: "/collections/4.png" },
+    ];
+    const max = (images.length - 1) * 440;
+    const start = 256;
+    let left = start;
+    const [springs, api] = useSpring(() => ({
+        x: left,
+    }), []);
+
+    const handleRightClick = () => {
+        api.start({
+            config: {
+                mass: 1,
+                friction: 15,
+                tension: 100,
+            },
+            to: {
+                x: left = left > start - max ? left - 440 : start,
+            },
+        });
+        //console.log(`Move Right: ${left}`);
+    }
+    const handleLeftClick = () => {
+        api.start({
+            config: {
+                mass: 1,
+                friction: 15,
+                tension: 100,
+            },
+            to: {
+                x: left = left < start ? left + 440 : start - max,
+            },
+        });
+        //console.log(`Move Left: ${left}`);
+    }
+
     return (
-        <div className="absolute top-36 left-64 flex overflow-clip shrink-0 min-w-[1511px]">
+        <div className="window absolute top-36 h-full w-[868px] overflow-hidden">
+            <animated.div
+                onClick={handleLeftClick}
+                style={{
+                    ...springs
+                }}
+                className="roll flex gap-7 absolute left-64">
 
-            {images.map((image) => {
-                return (
-                    <div key={image.id}>
-                        <div>
-                            Lunar Palace:(ft. Kanye west)
+                {images.map((image) => {
+                    return (
+                        <div key={image.id} className="shrink-0 rounded-lg">
+                            <div>
+                                Lunar Palace:(ft. Kanye west)
+                            </div>
+                            <Image
+                                src={image.url}
+                                alt=""
+                                width={412}
+                                height={630}
+                            />
                         </div>
-                        <Image
-                            src={image.url}
-                            alt=""
-                            //fill
-                            className="object-cover"
-                            width={412}
-                            height={630}
-                        />
-                    </div>
-                )
-            })}
+                    )
+                })}
 
+            </animated.div>
 
         </div>
     )
